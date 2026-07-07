e3948afa152da93592a3c1ddb2956b14306654f0
mamadousy1254 <tipox1254@gmail.com>
2026-07-07 17:02:01 +0000

    Changes

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   
   images: {
     remotePatterns: [
-    
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
